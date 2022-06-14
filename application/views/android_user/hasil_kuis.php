0329fb10e4dd59111836e182c31c8c0884c37c82
lahar91 <androrif28@gmail.com>
2022-06-14 21:21:17 +0700

    test display kuis 9

--- application/views/android_user/hasil_kuis.php
+++ application/views/android_user/hasil_kuis.php
@@ -1,15 +1,14 @@
-    <div class="col-lg-12">
-        <div class="card card-primary">
-            <div class="card-header"></div>
-            <div class="mx-auto">
-                <h4 class="ml-1">Score anda :</h4>
-                <h1 class="ml-1 mb-2 mt-2" style="font-size: 100px;"><?= $this->session->userdata('snilai'); ?></h1>
+<div class="col-lg-7">
+    <div class="card card-primary">
+        <div class="card-header"></div>
+        <div class="mx-auto">
+            <h4 class="ml-1">Score anda :</h4>
+            <h1 class="ml-1 mb-2 mt-2" style="font-size: 100px;"><?= $this->session->userdata('snilai'); ?></h1>
 
-            </div>
-            <div class="mx-auto mb-2">
-                <a href="<?= base_url('user/kuis') ?>" class="btn btn-outline-primary  btn-lg">Selesai</a>
-            </div>
+        </div>
+        <div class="mx-auto mb-2">
+            <a href="<?= base_url('user/kuis') ?>" class="btn btn-outline-primary  btn-lg">Selesai</a>
         </div>
     </div>
-
     <?php session_destroy('snilai'); ?>
+</div>
