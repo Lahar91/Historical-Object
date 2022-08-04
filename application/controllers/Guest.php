7274c61adb83f6c9cc33357954d58c8e2235c24b
lahar91 <androrif28@gmail.com>
2022-08-04 15:51:17 +0700

    fix error guest android

--- application/controllers/Guest.php
+++ application/controllers/Guest.php
@@ -35,9 +35,9 @@ class Guest extends CI_Controller
     {
         
         if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == "com.rrd.ho") {
-            $this->session->userdata('android') == "com.rrd.ho"
-            if ($this->input->cookie('viewer', true) == null && $this->input->cookie('viewer', true) == "")
+            if ($this->input->cookie('viewer', true) == null && $this->input->cookie('viewer', true) == ""){
                 $this->_readviewer();
+            }
 
             $data['title'] = 'Dashboard';
             $data['artikel'] = $this->user->artikeldesc();
