da15b971c02f44ebdb25ed020b8597ea4bde5851
lahar91 <androrif28@gmail.com>
2022-08-22 11:34:38 +0700

    update test

--- application/models/M_user.php
+++ application/models/M_user.php
@@ -148,7 +148,7 @@ class M_user extends CI_Model
         $this->db->select('kuis_jawab.id_user as iduser, img, sum(nilai) as hasil, img, username');
         $this->db->from('kuis_jawab');
         $this->db->join('user', 'kuis_jawab.id_user = user.id_user', 'INNER');
-        $this->db->where('iduser', $data);
+        $this->db->where('id_user', $data);
         $this->db->group_by('iduser');
         return $this->db->get()->result();
 
